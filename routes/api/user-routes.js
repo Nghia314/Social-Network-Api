daa1ec1bf896fc453cc78dc8dc021ffde0db71a6
Nghia Le <davidle314@hotmail.com>
2022-05-21 12:54:57 -0700

    file update with route

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,1 +1,20 @@
-const
+const router = require('express').Router();
+
+const {
+    getAllUsers,
+    createUser,
+    getUsersById,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend
+} = require('../../controllers/user-controller');
+
+router.route('/').get(getAllUsers).post(createUser);
+
+router.route('/:id').get(getUsersById).put(updateUser).delete(deleteUser);
+
+router.route('/:id/friends/:friendId').post(addFriend).delete(deleteFriend);
+
+
+module.exports = router;
