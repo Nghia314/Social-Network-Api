76e479ab82d37b566dfcd0d47a6f31fb2dff098e
Nghia Le <davidle314@hotmail.com>
2022-05-23 00:27:58 -0700

    testing the route

--- server.js
+++ server.js
@@ -1,24 +1,26 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3001;
 const app = express();
-
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.urlencoded({
+  extended: true
+}));
 
-app.use(require('./routes'));
 
+
+const mongoose = require('mongoose');
 mongoose.connect(
-process.env.MONGODB_URI || 'mongodb://localhost/Social-Network-Api',
-{
-  useFindAndModify: false,
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+  process.env.MONGODB_URI || 'mongodb://localhost/Social-Network-Api', {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+mongoose.connection.on('connected', () =>
+  console.log('Connected to mongoDb Endpoint'));
 
- mongoose.set("debug", true);
+mongoose.connection.on('error', (err) => console.log(`Mongoose Disconnected error: ${err}`));
 
+
+mongoose.set("debug", true);
+app.use(require('./routes'));
 app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
-
-
